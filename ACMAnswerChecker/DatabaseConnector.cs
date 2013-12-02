36d9445d8a78e51f506ed59ee0698249e17e7f94
Lialosiu <lialosiu@gmail.com>
2013-12-02 19:44:39 +0800

    去掉些头

--- ACMAnswerChecker/DatabaseConnector.cs
+++ ACMAnswerChecker/DatabaseConnector.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Globalization;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
-using System.Collections;
 using MySql.Data.Types;
 
 namespace ACMAnswerChecker
